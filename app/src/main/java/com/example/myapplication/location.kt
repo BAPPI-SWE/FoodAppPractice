3f6db5eb4e2309b544dbd1c4b49d33af5ddff67f
Rakibul islam Bappi <bappi616@gmail.com>
2025-06-03 23:55:13 +0600

    location page Done

--- app/src/main/java/com/example/myapplication/location.kt
+++ app/src/main/java/com/example/myapplication/location.kt
@@ -1,16 +1,28 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.databinding.ActivityLocationBinding
 
 class location : AppCompatActivity() {
+    private val binding: ActivityLocationBinding by lazy {
+        ActivityLocationBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_location)
+        setContentView(binding.root)
+        val list = arrayOf("Hall 1","Hall 2", "hall 3")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
+        val autoCompleteTextView = binding.list3
+        autoCompleteTextView.setAdapter(adapter)
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -18,3 +30,6 @@ class location : AppCompatActivity() {
         }
     }
 }
+
+
+
--- app/src/main/java/com/example/myapplication/signup.kt
+++ app/src/main/java/com/example/myapplication/signup.kt
@@ -1,18 +1,26 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.databinding.ActivitySignupBinding
 
 class signup : AppCompatActivity() {
-
+private val binding: ActivitySignupBinding by lazy{
+    ActivitySignupBinding.inflate(layoutInflater)
+}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_signup)
+        setContentView(binding.root)
+        binding.button7.setOnClickListener(){
+            val intent = Intent(this,location::class.java )
+            startActivity(intent)
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
